9b35d9da4937166ced57672bbed4c695a9217f28
1unar-Eclipse <nightwingplaysyt@gmail.com>
2023-09-10 07:36:28 -0400

    Update definitions

--- definitions/latite.d.ts
+++ definitions/latite.d.ts
@@ -1,3 +1,5 @@
+type EventName = 'world-tick' | 'leave-game' | 'receive-chat' | 'send-chat' | 'render2d';
+
 interface LatiteEvent {
 }
 
@@ -15,15 +17,7 @@ interface ReceiveChatEvent extends LatiteEvent {
     xuid: string;
 }
 
-declare const enum MouseButton {
-    Left = 1,
-    Right = 2,
-    Middle = 3,
-    Scroll = 4
-}
-
 interface ClickEvent extends CancellableEvent {
-    button: MouseButton;
     isDown: boolean;
     mouseX: number;
     mouseY: number;
@@ -42,109 +36,19 @@ interface ScriptEvent extends LatiteEvent {
 }
 
 interface ClientEvents {
-    /**
-    * Called on every tick.
-    */
     "world-tick": LatiteEvent,
-    /**
-    * Called on the user leaving a world.
-    */
     "leave-game": LatiteEvent,
-    /**
-    * Called on every chat message received.
-    * 
-    * Listener:
-    * ```ts
-    * {
-    *   message: string,
-    *   sender: string,
-    *   xuid: string
-    * }
-    * ```
-    */
     "receive-chat": ReceiveChatEvent,
-    /**
-    * Called on every chat message sent. Cancellable - setting `cancel` to true will make the game not see the event.
-    * 
-    * Listener:
-    * ```ts
-    * {
-    *   message: string,
-    *   cancel: boolean
-    * }
-    * ```
-    */
     "send-chat": SendChatEvent,
-    /**
-    * Called on every frame; use this for 2D rendering.
-    */
     "render2d": LatiteEvent,
-    /**
-    * Called on every frame; use this for DirectX rendering.
-    */
-    "renderDX": LatiteEvent,
-    /**
-    * Called on every keyboard input. Cancellable - setting `cancel` to true will make the game not see the event.
-    * 
-    * Listener:
-    * ```ts
-    * {
-    *   isDown: boolean,
-    *   keyCode: KeyCode,
-    *   keyAsChar: string,
-    *   cancel: boolean
-    * }
-    * ```
-    */
     "key-press": KeyEvent,
-    /**
-    * Called on every (non-move) mouse input. Cancellable - setting `cancel` to true will make the game not see the event.
-    * 
-    * Listener:
-    * ```ts
-    * {
-    *   button: MouseButton,
-    *   isDown: boolean,
-    *   mouseX: number,
-    *   mouseY: number,
-    *   cancel: boolean
-    * }
-    * ```
-    */
     "click": ClickEvent,
-    /**
-    * Called on every frame; use this for 3D rendering.
-    */
     "render3d": LatiteEvent,
     /**
-    * Called on the game being minimized/closed.
-    */
+     * Whenever the game gets minimized or closed.
+     */
     "app-suspended": LatiteEvent,
-    /**
-    * Called on any script being loaded.
-    * 
-    * Listener:
-    * ```ts
-    * {
-    *   scriptName: string,
-    *   scriptVersion: string,
-    *   scriptAuthor: string
-    * }
-    * ```
-    */
     "load-script": ScriptEvent,
-    /**
-    * Called on any script being unloaded.
-    * 
-    * Listener:
-    * ```ts
-    * {
-    *   scriptName: string,
-    *   scriptVersion: string,
-    *   scriptAuthor: string
-    * }
-    * ```
-    */
     "unload-script": ScriptEvent,
 }
 
@@ -172,15 +76,10 @@ interface Latite {
      */
     getModuleManager() : ModuleManager;
 
-    /**
-     * Gets the command manager. Use this to register commands.
-     */
-    getCommandManager(): CommandManager;
-
      /**
-     * The Latite Client version. Example: "v2.0.0"
+     * The Latite Client version. Example: v1.4.0 (release), b1.4.0 (beta)
      */
-    readonly version: string;
+     version: string;
 }
 
 declare const client: Latite;
