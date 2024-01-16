0a2c08144776af856901a0173242f015a318fb89
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-01-16 12:29:32 -0500

    fix random error

--- src/modExtraThingsPrevent.ts
+++ src/modExtraThingsPrevent.ts
@@ -40,6 +40,7 @@ let timeCurrent: number;
 function prevent(button: number, eventCancel: boolean) {
     // default return cases
     if(notOnGalaxite()) return; // are you on galaxite
+    if(game.getLocalPlayer() === null) return; // are you in a game
     if(game.getLocalPlayer()?.getSelectedSlot() != 9) return; // are you on slot 9 (i am not finding out the extra things item id)
 
     // get use button - not cached because it might change mid-game
