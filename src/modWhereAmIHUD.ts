d8fe4181d72b79ae4b6fe896b85d5a29cf1aafef
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-03-11 16:42:25 -0400

    new line fix

--- src/modWhereAmIHUD.ts
+++ src/modWhereAmIHUD.ts
@@ -297,10 +297,9 @@ whereAmIHUD.on("text", () => {
             optionPodNamePrefix.getValue(), podName, optionPodNameSuffix.getValue(),
             NL,
             optionCommitIDPrefix.getValue(), commitID, optionCommitIDSuffix.getValue(),
-            NL,
             optionShulkerIDPrefix.getValue(), shulkerID, optionShulkerIDSuffix.getValue(), (
                 parkourUUID != ""
-                ? (NL + optionParkourUUIDPrefix.getValue(), parkourUUID, optionParkourUUIDSuffix.getValue())
+                ? (NL + optionParkourUUIDPrefix.getValue(), parkourUUID, optionParkourUUIDSuffix.getValue()) // NL in here to reduce potential trim
                 : ""
             )
         ); // no final NL since that's always the last data point
