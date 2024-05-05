e4c480a956d04fafca04cc84d60dfb2fe9d2006d
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-05-05 08:05:50 -0400

    Update version

--- src/modKitUI.ts
+++ src/modKitUI.ts
@@ -4,6 +4,7 @@
 Chronos: Icon + perk name
 Chronos (Advanced): Icon + advanced thing
 Rush: Lines for each in-game loot mod, icon + count
+  To-do: Determine the original amounts of each kit from this message
 Hyper Racers: Engine + track name
 Kit PvP: Icon + kit name
 */
