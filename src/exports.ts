a4a4f0e2ebfbc22e9ff945495988f83d54ccd5a5
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-05-12 11:08:53 -0400

    Patch notes went off-screen

--- src/exports.ts
+++ src/exports.ts
@@ -122,7 +122,8 @@ export let patchNotes = new Map([
         "- Fixed a bug (hopefully) where prestige icons occasionally caused the Compact Badges module to not work as expected\n" +
         "- Fixed WhereAmIHUD not properly handling ParkourUUID\n" +
         "- Various backend changes\n\n" +
-        "Remember to report any bugs you find! Ping @1unar_Eclipse on the Galaxite Discord or open an issue at https://github.com/1unar-Eclipse/GalaxiteUtils"
+        "Remember to report any bugs you find! Ping @1unar_Eclipse on the Galaxite Discord or open an issue at https://github.com/1unar-Eclipse/GalaxiteUtils \n" +
+        "(press your chat button to view full patch notes)"
     ],
     ["0.3.1", "REMINDER UPDATE THIS LATER"],
     ["0.4.0", "GalaxiteUtils has been updated to v0.4.0!\n" +
