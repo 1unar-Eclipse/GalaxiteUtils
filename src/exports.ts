7ae6777f5a0f8a18cf787ba974600b757efdd5cb
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-05-17 13:34:55 -0400

    Patch notes

--- src/exports.ts
+++ src/exports.ts
@@ -159,7 +159,7 @@ export const patchNotes = new Map([
         "- Hiding automatic /whereami responses is now handled using an option in the Global Settings module, not WhereAmIHUD\n" +
         '- The plugin now automatically downloads a resource pack that removes armor. Click the "Open Latite Folder" button next time you launch and import the pack there!\n' +
         "- Plugin update notifications no longer assume you use . as your command prefix\n" +
-        "- Compact Badges has been expanded into a full chat editor, with multiple new options\n" +
+        "- Compact Badges has been expanded into a full chat editor, with multiple new options including traditional badges\n" +
         "\nRemember to report any bugs you find! Ping @1unar_Eclipse on the Galaxite or Latite Discord or open an issue at https://github.com/1unar-Eclipse/GalaxiteUtils.\n" +
         "(press your chat button to view full patch notes)"
     ]
