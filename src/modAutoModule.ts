3b22444a877781396017ab6f0fbf90af5eeabd66
1unar-Eclipse <nightwingplaysyt@gmail.com>
2024-05-25 21:00:01 -0400

    Start on AutoModule

--- src/modAutoModule.ts
+++ src/modAutoModule.ts
@@ -4,4 +4,31 @@
 // - Alien Blast: Toggle Sprint
 // - Chronos: Coordinates
 
-// import { notOnGalaxite } from "./exports";
+import { notOnGalaxite } from "./exports";
+
+let modAutoModule = new Module(
+    "automodule",
+    "GXU: Auto Module Toggles",
+    "Automatically turns off specific modules in certain games",
+    KeyCode.None
+);
+modAutoModule.addBoolSetting(
+    "chronos-coordinates",
+    "Coordinates (Chronos)",
+    "Turns off Coordinates while in Chronos",
+    true
+);
+modAutoModule.addBoolSetting(
+    "entity-togglesprint",
+    "Toggle Sprint (The Entity)",
+    "Turns off Toggle Sprint while in The Entity",
+    false
+);
+modAutoModule.addBoolSetting(
+    "alienblast-togglesprint",
+    "Toggle Sprint (Alien Blast)",
+    "Turns off Toggle Sprintt while in Alien Blast",
+    false
+);
+// client.getModuleManager().registerModule(modAutoModule);
+
